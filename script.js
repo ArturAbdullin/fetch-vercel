cff5a2d1096295736e44a0664dc6dde9988f37e8
Artur Abdullin <artur.abdullin@gmail.com>
2022-11-24 19:17:27 +0300

    feat: add ugly styling

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-const fetchButton = document.getElementById("fetch-data");
+const fetchButton = document.getElementById("section-data__fetch-button");
 
-const printField = document.querySelector(".print-data");
+const tableBody = document.querySelector(".section-data__table-body");
+
+const MemberModel = {
+  id: "string",
+  name: "string",
+  age: 1,
+  email: "string",
+};
+
+const createTableRow = (...cells) => {
+  const row = document.createElement("tr");
+
+  for (const cell of cells) {
+    const td = document.createElement("td");
+    td.innerHTML = cell;
+    row.appendChild(td);
+  }
+
+  return row;
+};
 
 const fetchData = async () => {
   const response = await fetch(
     "https://express-deploy-vercel.vercel.app/members"
   );
-  const data = response.json();
-  console.log(data);
+
+  /**
+   * @type {MemberModel[]}
+   */
+  const members = await response.json();
+
+  for (const { id, name, age, email } of members) {
+    tableBody.appendChild(createTableRow(id, name, age, email));
+  }
 };
 
 fetchButton.addEventListener("click", fetchData);
